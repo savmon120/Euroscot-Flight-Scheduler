1a1510f3614ec05dab3cd45cc866f985422d9dd9
Sav <85038851+savmon120@users.noreply.github.com>
2025-11-19 21:37:07 +0000

    Create FlightEntryBlock.php

---
+++ Plugin/Block/FlightEntryBlock.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\flight_scheduler\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a dispatcher-grade flight entry form block.
+ *
+ * @Block(
+ *   id = "flight_entry_block",
+ *   admin_label = @Translation("Flight Entry Form"),
+ * )
+ */
+class FlightEntryBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a new FlightEntryBlock instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => '<p>Dispatcher block is alive.</p>',
+    ];
+  }
+
+}
